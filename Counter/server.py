66249d735e2b65aba4c8fca8bd3c69539e13c3a4
Chris-Kenimer <ckenimer@hotmail.com>
2016-11-06 10:57:31 -0500

    Counter

---
+++ Counter/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, session, redirect, url_for
+app = Flask(__name__)
+app.secret_key = 'TonsOfVisitsAndLotsOfFun'
+@app.route('/')
+def index():
+    if session.get('counter'):
+        session['counter'] += 1
+        return render_template('index.html', counter=session['counter'])
+    else:
+        session['counter'] = 1
+        return render_template('index.html', counter=session['counter'])
+@app.route('/ninjas')
+def ninjas():
+    session['counter'] += 1
+    return redirect('/')
+@app.route('/hackers')
+def hackers():
+    session['counter'] = -1
+    return redirect('/')
+app.run(debug=True)

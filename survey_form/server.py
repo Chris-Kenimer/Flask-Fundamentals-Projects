e47534a71fb62547c6a97dce1f4369756fe7cda6
Chris-Kenimer <ckenimer@hotmail.com>
2016-11-07 19:09:15 -0500

    survey form validation

--- survey_form/server.py
+++ survey_form/server.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, session
 app = Flask(__name__)
+app.secret_key = 'TonsOfVisitsAndLotsOfFun'
 @app.route('/')
 def index():
   return render_template("survey.html")
 @app.route('/survey-submit', methods=['POST'])
 def submit_survey():
-    return render_template("survey_results.html", results=request.form)
+    session['name'] = request.form['name']
+    session['dojo'] = request.form['dojo']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    if len(request.form['name']) < 1:
+        flash('Name can not be blank')
+    if (len(request.form['comments']) > 120) or (len(request.form['comments']) < 5):
+        flash('Comment must between 5 and 120 characters')
+    return redirect('/')
 app.run(debug=True)
